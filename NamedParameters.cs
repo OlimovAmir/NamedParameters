69037376c749b6024ca30d9412e565d30c78f795
olimov Amir <olimov.amir@mail.ru>
2023-12-08 10:36:26 +0300

    Добавьте файлы проекта.

---
+++ NamedParameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class NamedParameters
+    {
+        static int Sum(int a, int b, bool enableLogging = false)
+        {
+            int result = a + b;
+            if (enableLogging)
+            {
+                Console.WriteLine("Value valiable a = " + a);
+                Console.WriteLine("Value valiable b = " + b);
+                Console.WriteLine("Value valiable result = " + result);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int result = Sum(1, 2, true);
+        }
+    }
+}
